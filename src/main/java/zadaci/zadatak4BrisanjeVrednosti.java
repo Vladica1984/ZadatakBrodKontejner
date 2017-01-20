ab27f7c5a4b361bb47e6695162859506e19824b0
vladica1984 <vladabeba84@gmail.com>
2017-01-20 19:22:29 +0100

    Uradjen zadatak brisanje vrednosti closes#9

--- src/main/java/zadaci/zadatak4BrisanjeVrednosti.java
+++ src/main/java/zadaci/zadatak4BrisanjeVrednosti.java
@@ -1,9 +1,16 @@
 package zadaci;
 
+import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
+import model.Brod;
+import model.Kontejner;
 
 import java.io.IOException;
+import java.util.List;
+
+import static zadaci.zadatak2DodavanjeVrednosti.brodDao;
+import static zadaci.zadatak2DodavanjeVrednosti.kontejnerDao;
 
 /**
  * Created by androiddevelopment on 20.1.17..
@@ -13,6 +20,21 @@ public class zadatak4BrisanjeVrednosti {
         ConnectionSource connectionSource = null;
         try {
             connectionSource = new JdbcConnectionSource("jdbc:sqlite:brodKontejner.db");
+
+            brodDao = DaoManager.createDao(connectionSource, Brod.class);
+            kontejnerDao = DaoManager.createDao(connectionSource, Kontejner.class);
+
+            List<Kontejner> kontejner = kontejnerDao.queryForAll();
+            for (Kontejner k: kontejner)
+                System.out.println("Kontejner = " + k);
+
+            List<Kontejner> pronadjenKontejner = kontejnerDao.queryForEq(Kontejner.POLJE_OZNAKA, "KP2");
+            Kontejner kontejnerZaBrisanje = pronadjenKontejner.get(0);
+            kontejnerDao.delete(kontejnerZaBrisanje);
+
+            kontejner = kontejnerDao.queryForAll();
+            for (Kontejner k: kontejner)
+                System.out.println("Kontejner = " + k);
 
 
         } catch (Exception e) {
