ab78a6819e233f154b0f78196c3a8323b06e2640
vladica1984 <vladabeba84@gmail.com>
2017-01-20 18:28:10 +0100

    Kreirane klase za zadatke closes#3

---
+++ src/main/java/zadaci/zadatak3IzmenaVrednosti.java
@@ -0,0 +1,21 @@
+package zadaci;
+
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+
+/**
+ * Created by androiddevelopment on 20.1.17..
+ */
+public class zadatak3IzmenaVrednosti {
+    public static void main(String[] args) {
+        ConnectionSource connectionSource = null;
+        try {
+            connectionSource = new JdbcConnectionSource("jdbc:sqlite:brodKontejner.db");
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
