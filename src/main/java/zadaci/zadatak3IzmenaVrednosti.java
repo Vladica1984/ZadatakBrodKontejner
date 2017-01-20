78c2fdc3c61e140ac1b71984d653ca0b18e298e8
vladica1984 <vladabeba84@gmail.com>
2017-01-20 19:16:55 +0100

    Uradjen zadatak izmena vrednosti closes#8

--- src/main/java/zadaci/zadatak3IzmenaVrednosti.java
+++ src/main/java/zadaci/zadatak3IzmenaVrednosti.java
@@ -1,9 +1,16 @@
 package zadaci;
 
+import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
+import model.Brod;
+import model.Kontejner;
 
 import java.io.IOException;
+import java.util.List;
+
+import static zadaci.zadatak2DodavanjeVrednosti.brodDao;
+import static zadaci.zadatak2DodavanjeVrednosti.kontejnerDao;
 
 /**
  * Created by androiddevelopment on 20.1.17..
@@ -13,6 +20,22 @@ public class zadatak3IzmenaVrednosti {
         ConnectionSource connectionSource = null;
         try {
             connectionSource = new JdbcConnectionSource("jdbc:sqlite:brodKontejner.db");
+
+            brodDao = DaoManager.createDao(connectionSource, Brod.class);
+            kontejnerDao = DaoManager.createDao(connectionSource, Kontejner.class);
+
+            List<Kontejner> kontejner = kontejnerDao.queryForAll();
+            for (Kontejner k: kontejner)
+                System.out.println("Kontejner = " + k);
+
+            List<Kontejner> pronadjenKontejner = kontejnerDao.queryForEq(Kontejner.POLJE_OPIS, "Morski plodovi");
+            Kontejner kontejnerZaIzmenu = pronadjenKontejner.get(0);
+            kontejnerZaIzmenu.setOpis("Plodovi mora");
+            kontejnerDao.update(kontejnerZaIzmenu);
+
+            kontejner = kontejnerDao.queryForAll();
+            for (Kontejner k: kontejner)
+                System.out.println("Kontejner = " + k);
 
 
         } catch (Exception e) {
